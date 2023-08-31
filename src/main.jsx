9492c13ec6de71817c38580766493fb2da8d412a
DennerDuarte <rm551938@fiap.com.br>
2023-08-30 23:16:21 -0300

    Projeto com Router criado

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+
+//Bloco de Rotas
+import {createBrowserRouter, RouterProvider } from 'react-router-dom';
+import EditarProdutos from './routes/EditarProdutos.jsx';
+import Home from './routes/home.jsx';
+import Produtos from './routes/Produtos.jsx';
+import Erro404 from './routes/Erro404.jsx';
+
+const router = createBrowserRouter([
+  {path: '/', element: <App/>, 
+    errorElement: <Erro404/>,
+    children: [
+      {path: '/', element: <Home/>},
+      {path: '/produtos', element: <Produtos/>},
+      {path: '/editar/produtos/:id', element: <EditarProdutos/>},
+    ]}
+]);
+
+//Bloco de Rotas
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router ={router}/>
+  </React.StrictMode>,
+)
