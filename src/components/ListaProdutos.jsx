5980a25474bc6d6a8873dbefd5694d0037cc7ecf
DennerDuarte <rm551938@fiap.com.br>
2023-09-06 23:20:29 -0300

    Arrumei os error

--- src/components/ListaProdutos.jsx
+++ src/components/ListaProdutos.jsx
@@ -4,6 +4,5 @@ export const ListaProdutos = [
     {id:2, nome:"Teclado", desc:"Teclado Gamer", img:"https://picsum.photos/50/40", preco:79},
     {id:3, nome:"Cadeira Gamer", desc:"Cadeira gamer articulada", img:"https://picsum.photos/100/100", preco:1500},
     {id:4, nome:"Fone Gamer", desc:"Fone gamer colorido", img:"https://picsum.photos/100/100", preco:300},
-    {id:5, nome:"Monitor", desc:"15'Itautec", img:"https://picsum.photos/100/100", preco:900},
     
 ];
