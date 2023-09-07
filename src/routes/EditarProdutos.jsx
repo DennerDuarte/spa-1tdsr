bed58e1ee47a6e1e04adc1a3ea1c8b5c17bc44f1
DennerDuarte <rm551938@fiap.com.br>
2023-09-06 22:43:01 -0300

    Apresentação do HOOK userState(), estilo com objects

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom"
 import { ListaProdutos } from "../components/ListaProdutos"
+import { useState } from "react";
 
 
 
@@ -9,14 +10,46 @@ export default function EditarProdutos() {
 
   const {id} = useParams();
 
-  const produtoFiltrado = ListaProdutos.filter( (item) => item.id === parseInt(id));
+  const produtoFiltrado = ListaProdutos.filter( (item) => item.id === parseInt(id)) [0];
+
+  const [produto, setProduto ] = useState({
+    id: produtoFiltrado.id,
+    nome: produtoFiltrado.nome,
+    desc: produtoFiltrado.desc,
+    img: produtoFiltrado.img,
+    preco: produtoFiltrado.preco
+  });
 
   return (
     <>
     <h1>EditarProdutos</h1>
-    <p>Produto Selecionado : {id} </p>
-    <p>Nome do produto: {produtoFiltrado[0].nome}</p>
-    <img src={produtoFiltrado[0].img} alt={produtoFiltrado[0].nome} />
+    <div>
+      <form>
+        <fieldset>
+          <legend>Produto Selecionado</legend>
+          <div>
+            <label htmlFor="idNome">Nome</label>
+            <input type="text" name="nome" id="idNome" value={produto.nome}/>
+          </div>
+          <div>
+            <label htmlFor="idDesc">Descrição</label>
+            <input type="text" name="desc" id="idDesc" value={produto.desc}/>
+          </div>
+          <div>
+            <label htmlFor="idImg">Imagem</label>
+            <input type="url" name="img" id="idImg" value={produto.img}/>
+          </div>
+          <div>
+            <label htmlFor="idPreco">Preço</label>
+            <input type="text" name="preco" id="idPreco" value={produto.preco}/>
+          </div>
+
+          <div>
+            <button onClick={()=> setProduto()}>Editar</button>
+          </div>
+        </fieldset>
+      </form>
+    </div>
     </>
   )
 }
